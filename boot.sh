395c823851260927b72036538552562dd26f916c
Kevin Alavik <kevin@alavik.se>
2023-12-25 12:33:42 +0100

    im tryign to fix submodules bare with me

---
+++ boot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+OS_NAME="nash"
+
+make -C limine
+make
+
+mkdir -p iso_root
+cp -v bin/"$OS_NAME" limine.cfg limine/limine-bios.sys \
+    limine/limine-bios-cd.bin limine/limine-uefi-cd.bin iso_root/
+
+mkdir -p iso_root/EFI/BOOT
+cp -v limine/BOOTX64.EFI iso_root/EFI/BOOT/
+cp -v limine/BOOTIA32.EFI iso_root/EFI/BOOT/
+
+
+xorriso -as mkisofs -b limine-bios-cd.bin \
+    -no-emul-boot -boot-load-size 4 -boot-info-table \
+    --efi-boot limine-uefi-cd.bin \
+    -efi-boot-part --efi-boot-image --protective-msdos-label \
+    iso_root -o image.iso
+
+./limine/limine bios-install image.iso
+rm -rf iso_root
+
+qemu-system-x86_64 -hda image.iso "${@}"  
